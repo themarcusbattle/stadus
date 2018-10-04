7b719c93200bcd50d0c13ead2711bd16c3d18398
Marcus Battle <themarcusbattle@gmail.com>
2018-10-04 00:55:30 -0400

    Added the intializing file to load weaverbird and other dependencies

---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: Weaverbird Starter Plugin
+ * Description: [Insert your description here]
+ * Author: Marcus Battle
+ * Author URI: https://marcusbattle.com
+ */
+
+require_once( 'vendor/autoload.php' );
